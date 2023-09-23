deff39bfc496af24c491595d36b2c3303829a412
marieoc <marie.occelli13@gmail.com>
2023-09-23 17:28:28 +0200

    added mobile navbar -- global width needs to be fixed

--- components/Nav/Nav.jsx
+++ components/Nav/Nav.jsx
@@ -5,23 +5,65 @@ import Image from 'next/image'
 import Link from 'next/link'
 
 const Nav = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  }
 
   return (
     <header className='flex space-between'>
-      <a className='logo_wrapper'>
+      <Link href="/" className='logo_wrapper'>
         <Image
           src="/img/logo1.png"
           alt="Logo"
           width={100}
           height={100}
         />
-      </a>
+      </Link>
 
       {/* MOBILE NAVBAR */}
-     {/*  <nav>
-        
-      </nav> */}
+      <div className="mobile_navbar__wrapper">
+        <button
+          className={`burger ${isMobileMenuOpen ? "active" : ""}`} 
+          onClick={toggleMobileMenu}
+        >
+          <span></span>
+        </button>
+
+        <nav className={`mobile_navbar w-full flex flex-col space-around ${isMobileMenuOpen ? "active" : ""}`}>
+
+          <ul className="w-full flex flex-col flex-center">
+            <li>
+              <Link href="/" onClick={() => setIsMobileMenuOpen(false)}>accueil</Link>
+            </li>
+            <li>
+              <Link href="">à propos</Link>
+            </li>
+            <li>
+              <Link href="">actualités</Link>
+            </li>
+            <li>
+              <Link href="">entraînements</Link>
+            </li>
+            <li>
+              <Link href="">galerie</Link>
+            </li>
+          </ul>
+
+          <div className="contact_btn_wrapper flex flex-center w-full">
+            <Link 
+              className="border-button-effect" 
+              href="/contact"
+              onClick={() => setIsMobileMenuOpen(false)}
+            >
+              Contact
+            </Link>
+          </div>
+
+        </nav>
+
+      </div>
 
       {/* DESKTOP NAVBAR */}
       <nav className='desktop_navbar flex flex-wrap space-between'>
@@ -36,7 +78,7 @@ const Nav = () => {
             <Link className='nav-item' href="">Entraînements</Link>
           </li>
           <li>
-            <Link className='nav-item' href="">Gallerie</Link>
+            <Link className='nav-item' href="">Galerie</Link>
           </li>
         </ul>
 
